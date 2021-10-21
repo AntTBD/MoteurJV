69d4bb7bc333b3cb657afc5a7687bd2b498f9975
AntTBD <antbd@free.fr>
2021-10-21 12:36:05 -0400

    complete contact resolver (+change param to use vector)

--- ParticleContactResolver.cpp
+++ ParticleContactResolver.cpp
@@ -1,5 +1,51 @@
 #include "ParticleContactResolver.h"
+#include <limits>
 
-void ParticleContactResolver::resolveContacts(ParticleContact* contactArray, unsigned int numContact, float duration)
+/// <summary>
+/// Résoudre les contacts les plus important d’abord !<para />
+/// ---<para />
+/// 1. Calculer les vélocités relatives de tout les contacts et conserver la plus petite valeur<para />
+/// ==> Si la plus petite valeur est plus grand que 0, alors tout les contacts sont résolue<para />
+/// 2. Gérer la collision du contacts(Impulsion + interpénétration)<para />
+/// 3. Recommencer à l’étape 1<para />
+/// ---<para />
+/// *Afin de limiter le temps nécessaire au calcul, on limitera le nombre d’itération(ex : 2 fois le nombre de contacts)
+/// </summary>
+/// <param name="contactArray">list des contacts</param>
+/// <param name="duration"></param>
+void ParticleContactResolver::resolveContacts(std::vector<ParticleContact*> contactArray, float duration)
 {
+	//  on limite le nombre d’itération (ex : 2 fois le nombre de contacts)
+	this->maxIteration = 2 * contactArray.size();
+
+	float minSeparatingVelocity;
+	float minContactIndex;
+	float tempSeparatingVelocity;
+
+	int iteration = 0;
+	while (iteration < this->maxIteration) // tant qu'on a pas atteint le nombre max d'iterations
+	{
+		// Calculer les vélocités relatives de tout les contacts et conserver la plus petite valeur
+		minSeparatingVelocity = std::numeric_limits<float>::infinity(); // on initialise le min à l'infini
+		minContactIndex = 0;
+
+		for (int i = 0; i < contactArray.size(); i++)
+		{
+			tempSeparatingVelocity = contactArray[i]->calculateSeparatingVelocity();
+			if (tempSeparatingVelocity < minSeparatingVelocity) // on conserve la plus petite valeur
+			{
+				minSeparatingVelocity = tempSeparatingVelocity;
+				minContactIndex = i;
+			}
+		}
+
+		// Si la plus petite valeur est plus grand que 0, alors tout les contacts sont résolue
+		if (minSeparatingVelocity >= 0)
+		{
+			break;
+		}
+		// resolution du contact (Impulsion + interpénétration)
+		contactArray[minContactIndex]->resolve(duration);
+		iteration++;
+	}
 }
--- ParticleContactResolver.h
+++ ParticleContactResolver.h
@@ -1,19 +1,19 @@
 #pragma once
 
 #include "ParticleContact.h"
+#include <vector>
 
 class ParticleContactResolver
 {
 protected:
 
 	// number of allowed iteration
-	unsigned int iteration;
+	unsigned int maxIteration;
 
 public:
-
 	// take an array of contact
 	// iterate on each contact according to their separatingVelocity
 	// stop when all contacts are solved or when max iteration is reached
-	void resolveContacts(ParticleContact* contactArray, unsigned int numContact, float duration);
+	void resolveContacts(std::vector<ParticleContact*> contactArray, float duration);
 };
 
