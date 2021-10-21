b42c5f45dc235a36a819fe1191f6734c3257a6b6
AntTBD <antbd@free.fr>
2021-10-21 11:34:38 -0400

    add drag force (agit comme force de frottement

---
+++ ParticleDrag.cpp
@@ -0,0 +1,10 @@
+#include "ParticleDrag.h"
+
+ParticleDrag::ParticleDrag(float k1, float k2) : m_k1(k1), m_k2(k2) {}
+
+ParticleDrag::~ParticleDrag() {}
+
+void ParticleDrag::updateForce(Particle* particle, float duration)
+{
+	particle->AddForce(particle->GetSpeed().Normalize() * -(m_k1 * particle->GetSpeed().Magnitude() + m_k2 * particle->GetSpeed().SquaredMagnitude()));
+}
