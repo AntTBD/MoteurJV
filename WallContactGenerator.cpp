bd938fca373fd17d7db5078913a28c360bb55e6d
xNaRKoZz <t.connan.nhan98@gmail.com>
2021-10-21 20:05:06 -0400

    WallContactGenerator

--- WallContactGenerator.cpp
+++ WallContactGenerator.cpp
@@ -1,1 +1,23 @@
 #include "WallContactGenerator.h"
+
+WallContactGenerator::WallContactGenerator(Particle* particle):
+	particle(particle)
+{
+	this->wall = new Particle(Vector3(), Vector3(), 1.0f/99.0f, 0);
+}
+
+
+
+WallContactGenerator::~WallContactGenerator()
+{
+}
+
+unsigned int WallContactGenerator::addContact(ParticleContact* contact, unsigned int limit) const
+{
+	if (this->particle->GetPosition().GetY() <= 0) {
+		contact = new ParticleContact(*this->particle, *this->wall, 0, -this->particle->GetPosition().GetY());
+		return 1;
+	}
+
+	return 0;
+}
--- WallContactGenerator.h
+++ WallContactGenerator.h
@@ -5,6 +5,16 @@
 class WallContactGenerator :
     public ParticleContactGenerator
 {
-    // ...
+private:
+    Particle* particle;
+    Particle* wall;
+public: 
+    WallContactGenerator(Particle* particle);
+    ~WallContactGenerator();
+    
+
+    // Fill ParticleContact with information form particles and contact generator.
+    // Contact is different wether the disitance is shaorter or greater than the length
+    unsigned int addContact(ParticleContact* contact, unsigned int limit) const;
 };
 
