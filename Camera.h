b42110659bbe3d6dcc8983f4dcad090410ad85f9
AntTBD <antbd@free.fr>
2021-11-11 23:25:29 -0500

    add general include file

--- Camera.h
+++ Camera.h
@@ -5,25 +5,7 @@
 
 #include <math.h>
 
-// Include ImGui with OpenGL and GLFW
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <stdio.h>
-
-// Include GLFW
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <GLES2/gl2.h>
-#endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
-
-// Include GLM
-#include <glm/glm.hpp>
-#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
-#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+#include "includesUI.h"
 
 /// <summary>
 /// Orbital Camera
