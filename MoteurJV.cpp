1cd80ab75f273acfa26f3b755b9b1ec369376bf1
AntTBD <antbd@free.fr>
2021-11-13 23:38:11 -0500

    works with vs

--- MoteurJV.cpp
+++ MoteurJV.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <chrono>
 #include <thread>
 #include <vector>
@@ -8,32 +8,32 @@
 #include "Simulator.h"
 #include "src/Math/Matrix33.h"
 #include "src/Math/Matrix34.h"
-#include "src/Math/Quaternion.h"
+#include "src/Math/Quaternion.h"*/
 
 #include "src/EngineManager.h"
-
+/*
 /// <summary>
 /// Thread for 3D user interface face manager
 /// </summary>
 /// <param name="sim">Pointer gathering all the characteristics of the particles of the scene</param>
 int UI(Simulator* sim)
 {
-    /*GUI gui(sim);
-    int gui_init = gui.init();
-    if(gui_init != 0)
-        return gui_init;
-    gui.update();
-    gui.end();*/
+    //GUI gui(sim);
+    //int gui_init = gui.init();
+    //if(gui_init != 0)
+    //    return gui_init;
+    //gui.update();
+    //gui.end();
 
     EngineManager::getInstance().run();
 
     return 0;
-}
+}*/
 
 int main(int, char**)
 {
-    Simulator sim;
-    std::thread threadUI(UI, &sim);     // spawn new ui thread
+    //Simulator sim;
+    //std::thread threadUI(UI, &sim);     // spawn new ui thread
 
     /*float values[12] = { 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f };
     Matrix34 A = Matrix34(values);
@@ -43,7 +43,8 @@ int main(int, char**)
     std::cout << A.TransformDirection(vec) << std::endl;
 */
     // synchronize threads:
-    threadUI.join();               // pauses until second finishes
+    //threadUI.join();               // pauses until second finishes
 
+    EngineManager::getInstance().run();
     return 0;
 }
